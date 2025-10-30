587fd4cf07e07d0bb23799bf3f33f1ef14e177c5
Den <dend51108@gmail.com>
2025-10-30 18:15:05 +0300

    + new structure

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+    const slideInterval = 3000;
+
+    function nextSlide() {
+        slides[currentSlide].classList.remove("active");
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add("active");
+    }
+
+    if (slides.length > 1) {
+        setInterval(nextSlide, slideInterval);
+    }
+});
